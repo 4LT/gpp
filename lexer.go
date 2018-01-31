0bf66e21a2a06612119ef65817419d5ae4fc797c
seth <rader.seth@gmail.com>
2018-01-31 18:09:54 -0500

    First commit - added lexer

---
+++ lexer.go
@@ -0,0 +1,186 @@
+package gpp
+
+import (
+    "bufio"
+    "strings"
+    "fmt"
+    "regexp"
+)
+
+type Token struct {
+    Type string
+    Value string
+}
+
+type TokenMatcher struct {
+    Regexp *regexp.Regexp
+    TokenType string
+}
+
+type ParseError struct {
+    Source string
+    Line int
+    Message string
+}
+
+// "(.*)"\pZ+(.*)$
+var matchLangLine *regexp.Regexp = regexp.MustCompile("\"(.*)\"\\pZ+(.*)$")
+
+// ^[^"\pZ]+$
+var matchTokenType *regexp.Regexp = regexp.MustCompile("^[^\"\\pZ]+$")
+
+var specialTypes = []string{"@skip", "@fail", "@conc"}
+
+func (e ParseError) Error() string {
+    if e.Line < 1 {
+        return e.Message
+    } else {
+        return fmt.Sprintf("%v: Line %v: %v", e.Source, e.Line, e.Message)
+    }
+}
+
+func ParseLang(langScanner *bufio.Scanner) ([]TokenMatcher, error) {
+    var lang []TokenMatcher
+    var err error
+    lineNum := 0
+
+    for langScanner.Scan() && err == nil {
+        lineNum++
+        line := langScanner.Text()
+        line = strings.TrimSpace(line)
+
+        if len(line) > 0 && line[0] != '#' {
+            submatchIdxs := matchLangLine.FindStringSubmatchIndex(line)
+            var reStr, tokenType string
+            var re *regexp.Regexp
+            var reErr error
+
+            if len(submatchIdxs) != 6 || submatchIdxs[0] != 0 ||
+                    submatchIdxs[1] != len(line) {
+                err = ParseError{
+                    "Lang def",
+                    lineNum,
+                    "Misformatted line",
+                }
+            }
+
+            if err == nil {
+                reStr = line[submatchIdxs[2]:submatchIdxs[3]]
+                tokenType = line[submatchIdxs[4]:submatchIdxs[5]]
+                re, reErr = regexp.Compile("^" + reStr)
+            }
+
+            if err != nil {
+                // do nothing
+            } else if reErr != nil {
+                err = ParseError{
+                    "Lang def",
+                    lineNum,
+                    fmt.Sprintf("Regexp:\n\t%v", reErr),
+                }
+            } else if !matchTokenType.MatchString(tokenType) {
+                err = ParseError{
+                    "Lang def",
+                    lineNum,
+                    "Misformatted token type",
+                }
+            } else if re.NumSubexp() > 1 {
+                err = ParseError{
+                    "Lang def",
+                    lineNum,
+                    "Multiple capturing groups exist in regexp",
+                }
+            } else {
+                lang = append(lang, TokenMatcher{re, tokenType})
+            }
+
+            if err == nil && tokenType[0] == '@' {
+                foundType := false
+
+                for _, specialType := range specialTypes {
+                    if specialType == tokenType {
+                        foundType = true
+                    }
+                }
+
+                if !foundType {
+                    err = ParseError{
+                        "Lang def",
+                        lineNum,
+                        "Token type " + tokenType + " is reserved",
+                    }
+                }
+            }
+        }
+    }
+
+    if err == nil {
+        err = langScanner.Err()
+    }
+
+    return lang, err
+}
+
+func lexLine(lang []TokenMatcher, tokStream chan Token, lineSlice string,
+lineNum int) error {
+
+    var err error
+
+    for len(lineSlice) > 0 {
+        for i, matcher := range lang {
+            submatches := matcher.Regexp.FindStringSubmatch(lineSlice)
+
+            if len(submatches) > 0 {
+                var tokenValue string
+
+                if len(submatches) > 1 {
+                    tokenValue = submatches[1]
+                } else {
+                    tokenValue = submatches[0]
+                }
+
+                if matcher.TokenType == "@skip" {
+                    // do nothing
+                } else if matcher.TokenType == "@fail" {
+                    err = ParseError{
+                        "Input",
+                        lineNum,
+                        "Found illegal token: " + tokenValue,
+                    }
+                } else {
+                    tokStream <- Token{ matcher.TokenType, tokenValue }
+                }
+
+                if len(submatches[0]) > 0 {
+                    lineSlice = lineSlice[len(submatches[0]):]
+                } else {
+                    lineSlice = lineSlice[1:]
+                }
+                break
+            } else if i == len(lang) - 1 {
+                err = ParseError{
+                    "Input",
+                    lineNum,
+                    "Could not extract token from " + lineSlice,
+                }
+            }
+        }
+    }
+
+    return err
+}
+
+func Lex(lang []TokenMatcher, input *bufio.Scanner, tokStream chan Token)  {
+    lineNum := 0
+    var err error
+
+    for err == nil && input.Scan() {
+        lineNum++
+        line := input.Text()
+        err = lexLine(lang, tokStream, line, lineNum)
+    }
+
+    close(tokStream)
+}
+
+
---
+++ lexer_test.go
@@ -0,0 +1,171 @@
+package gpp
+
+import (
+    "testing"
+    "bytes"
+    "bufio"
+)
+
+func TestParseLang(t *testing.T) {
+    var buffer bytes.Buffer
+    buffer.WriteString(
+        "#comment\n" +
+        "  \n" +
+        "\"\"(.*)\"\" string\n" +
+        "\"{\" @conc\n" +
+        "\t#indented comment\n" +
+        "  \"[0-9]*\" int  ")
+    lang, err := ParseLang(bufio.NewScanner(&buffer))
+
+    if err != nil {
+        t.Errorf("Error returned by ParseLang: %v", err)
+    } else if len(lang) != 3 {
+        t.Errorf("Expected language length: %v, got: %v", 3, len(lang))
+    } else {
+        tokTypeErr := "Expected token type: %v, got: %v"
+        regexpErr := "Expected regexp: %v, got: %v"
+        tokenType := lang[0].TokenType
+        regexp := lang[0].Regexp.String()
+
+        if tokenType != "string" {
+            t.Errorf(tokTypeErr, "string", tokenType)
+        }
+
+        if regexp != "^\"(.*)\"" {
+            t.Errorf(regexpErr, "^\"(.*)\"", regexp)
+        }
+
+        tokenType = lang[1].TokenType
+        regexp = lang[1].Regexp.String()
+
+        if tokenType != "@conc" {
+            t.Errorf(tokTypeErr, "@conc", tokenType)
+        }
+
+        if regexp != "^{" {
+            t.Errorf(regexpErr, "^{", regexp)
+        }
+
+        tokenType = lang[2].TokenType
+        regexp = lang[2].Regexp.String()
+
+        if tokenType != "int" {
+            t.Errorf(tokTypeErr, "int", tokenType)
+        }
+
+        if regexp != "^[0-9]*" {
+            t.Errorf(regexpErr, "^[0-9]*", regexp)
+        }
+    }
+}
+
+func TestParseLangErrorNoInitQuote(t *testing.T) {
+    var buffer bytes.Buffer
+    buffer.WriteString("line \"must start with a\" quote")
+    _, err := ParseLang(bufio.NewScanner(&buffer))
+
+    if err == nil {
+        t.Error("Line not starting with quote did not cause error")
+    }
+}
+
+func TestParseLangErrorNoEndQuote(t *testing.T) {
+    var buffer bytes.Buffer
+    buffer.WriteString("\"regexp must have an ending quote")
+    _, err := ParseLang(bufio.NewScanner(&buffer))
+
+    if err == nil {
+        t.Error("Regexp missing end quote did not cause error")
+    }
+}
+
+func TestParseLangErrorMultipleTokensAfterRegexp(t *testing.T) {
+    var buffer bytes.Buffer
+    buffer.WriteString("\"foo\" bar baz")
+    _, err := ParseLang(bufio.NewScanner(&buffer))
+
+    if err == nil {
+        t.Error("Multiple tokens after regexp did not cause error")
+    }
+}
+
+func TestParseLangErrorBadRegexp(t *testing.T) {
+    var buffer bytes.Buffer
+    buffer.WriteString("\".*)\" badregexp")
+    _, err := ParseLang(bufio.NewScanner(&buffer))
+
+    if err == nil {
+        t.Error("Badly formatted regexp did not cause error")
+    }
+}
+
+func TestParseLangErrorReservedType(t *testing.T) {
+    var buffer bytes.Buffer
+    buffer.WriteString("\"foo\" @strange")
+    _, err := ParseLang(bufio.NewScanner(&buffer))
+
+    if err == nil {
+        t.Error("Use of reserved token type did not cause error")
+    }
+}
+
+func TestParseLangErrorMultipleCapGroups(t *testing.T) {
+    var buffer bytes.Buffer
+    buffer.WriteString("\"(foo)(bar)\" foobar")
+    _, err := ParseLang(bufio.NewScanner(&buffer))
+
+    if err == nil {
+        t.Error("Multiple capturing groups did not cause error")
+    }
+}
+
+func TestLex(t *testing.T) {
+    var langBuf bytes.Buffer
+    var inputBuf bytes.Buffer
+    tokStream := make(chan Token, 20)
+    expectedValues := []Token{
+        {"string", "Hello"},
+        {"int", "367"},
+        {"int", "13"},
+        {"string", "Good bye"},
+    }
+
+    langBuf.WriteString(
+        "\"\"(.*)\"\"   string\n" +
+        "\"[0-9]+\"     int\n" +
+        "\"\\pZ*\"      @skip")
+    lang, langErr := ParseLang(bufio.NewScanner(&langBuf))
+
+    if (langErr != nil) {
+        t.Error("Language def error: " + langErr.Error())
+        return
+    }
+
+    inputBuf.WriteString("\"Hello\"   \t367\n13  \"Good bye\"   ")
+    go Lex(lang, bufio.NewScanner(&inputBuf), tokStream)
+
+    tokIdx := 0
+    for tok := range tokStream {
+        if tokIdx > 3 {
+            t.Error("Received more than expected number of tokens")
+        }
+
+        expectedTok := expectedValues[tokIdx]
+
+        if (tok.Type != expectedTok.Type) {
+            t.Errorf("Expected token type: %v, got: %v", tok.Type,
+                expectedTok.Type)
+        }
+
+        if (tok.Value != expectedTok.Value) {
+            t.Errorf("Expected token value: %v, got: %v", tok.Value,
+                expectedTok.Value)
+        }
+
+        tokIdx++
+    }
+
+    if tokIdx < 3 {
+        t.Error("Received fewer than expected number of tokens")
+    }
+}
